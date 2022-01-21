wahello/codeblocks

commit 10f2cfd3e30f1980b7d3bf3ba9c742b02ebd992c

* Project importer: Check if menu entry exists before creating it (ticket #98, thanks TurboSnail). Applied slightly modified patch (formatting).
git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@12597 2a5c6006-c6dd-42ca-98ab-0921f2732cef

src/plugins/projectsimporter/projectsimporter.cpp
@@ -66,22 +66,44 @@ void ProjectsImporter::BuildMenu(wxMenuBar* menuBar)
         return;
 
     wxMenu* fileMenu = menuBar->GetMenu(0);
-    if (fileMenu)
+    if (!fileMenu)
+        return;
+
+    wxMenuItem* importMenuItem;
+    const int importMenuId = fileMenu->FindItem(_("&Import project"));
+    if (importMenuId == wxNOT_FOUND)
     {
-        int menuId = 0, id = 0;
         wxMenuItemList menuItems = fileMenu->GetMenuItems();
-        menuId = fileMenu->FindItem(_("R&ecent files"));
+        const int menuId = fileMenu->FindItem(_("R&ecent files"));
         wxMenuItem* recentFileItem = fileMenu->FindItem(menuId);
-        id = menuItems.IndexOf(recentFileItem);
+        int id = menuItems.IndexOf(recentFileItem);
         if (id == wxNOT_FOUND)
             id = 7;
         else
             ++id;
 
         // The position is hard-coded to "Recent Files" menu. Please adjust it if necessary
-        fileMenu->Insert(++id, wxNewId(), _("&Import project"), m_Menu);
+        importMenuItem = fileMenu->Insert(++id, wxNewId(), _("&Import project"), new wxMenu());
         fileMenu->InsertSeparator(++id);
     }
+    else
+    {
+        importMenuItem = fileMenu->FindItem(importMenuId);
+    }
+
+    if (!importMenuItem)
+        return;
+
+    if (!importMenuItem->IsSubMenu())
+        importMenuItem->SetSubMenu(new wxMenu());
+
+    wxMenu* importSubMenu = importMenuItem->GetSubMenu();
+    if (importSubMenu->GetMenuItemCount())
+        importSubMenu->AppendSeparator();
+
+    wxMenuItemList m_List = m_Menu->GetMenuItems();
+    for (wxMenuItemList::iterator it = m_List.begin(); it != m_List.end(); ++it)
+        importSubMenu->Append(*it);
 }
 
 bool ProjectsImporter::CanHandleFile(const wxString& filename) const

commit 1230be14f5db4210723f8112a3ef3b760ff6d169

* Fix crash on MAC introduced in [r12597] (thanks Xaviou for reporting and testing).

src/plugins/projectsimporter/projectsimporter.cpp
@@ -61,8 +61,8 @@ void ProjectsImporter::BuildMenu(wxMenuBar* menuBar)
     if (!IsAttached() || !menuBar)
         return;
 
-    m_Menu = Manager::Get()->LoadMenu(_T("project_import_menu"), false);
-    if (!m_Menu)
+    wxMenu* menu = Manager::Get()->LoadMenu("project_import_menu", false);
+    if (!menu)
         return;
 
     wxMenu* fileMenu = menuBar->GetMenu(0);
@@ -101,9 +101,11 @@ void ProjectsImporter::BuildMenu(wxMenuBar* menuBar)
     if (importSubMenu->GetMenuItemCount())
         importSubMenu->AppendSeparator();
 
-    wxMenuItemList m_List = m_Menu->GetMenuItems();
+    wxMenuItemList m_List = menu->GetMenuItems();
     for (wxMenuItemList::iterator it = m_List.begin(); it != m_List.end(); ++it)
-        importSubMenu->Append(*it);
+        importSubMenu->Append(menu->Remove(*it));
+
+    delete menu;
 }
 
 bool ProjectsImporter::CanHandleFile(const wxString& filename) const

src/plugins/projectsimporter/projectsimporter.h
@@ -20,8 +20,6 @@ class ProjectsImporter : public cbMimePlugin
     private:
         int LoadProject(const wxString& filename);
         int LoadWorkspace(const wxString& filename);
-
-        wxMenu* m_Menu;
 };
 
 #endif // PROJECTSIMPORTER_H